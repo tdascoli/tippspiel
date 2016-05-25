100b0e2d3cc1a935e01e0c86a2b94bffebbdcadc
Thomas D'Ascoli <thomas@dasco.li>
2016-05-25 10:54:49 +0200

    yo skeleton

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tippspiel
+# Ang-Generator
+TOKEN https://soccerama.pro/settings#/api 2P1szKhpGrIg2rvYyiwM6WvVNETTOi2cjW7cL1WmBWLS7ELMcx2OQFkEzk9W
---
+++ app/app.js
@@ -0,0 +1,26 @@
+(function() {
+	'use strict';
+
+	/**
+	* @ngdoc index
+	* @name app
+	* @description
+	* # app
+	*
+	* Main module of the application.
+	*/
+
+	angular.module('tippspiel', [
+		'ngResource',
+		'ngAria',
+		 'ngMaterial',
+		'ngMdIcons',
+		'ngMessages',
+		'ngCookies',
+		'ngAnimate',
+		'ngSanitize',
+		'ui.router',
+		'home',
+	]);
+
+})();
